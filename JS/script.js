65c06d2936e99d8266e082b0fd4df08cfe23c674
OscarAvendano <andres.101014@gmail.com>
2023-06-15 21:58:56 -0500

    se sube el resultado final

--- JS/script.js
+++ JS/script.js
@@ -76,3 +76,45 @@ function cerrarPopup() {
     var popupContainer = document.getElementById('popupContainer');
     popupContainer.style.display = 'none';
 }
+// Función para mostrar el popup
+function mostrarPopup() {
+    document.getElementById("popupContainer").style.display = "flex";
+}
+function salirMenu(){
+    window.location.href = "../VIEWS/admin.html";
+}
+// Función para cerrar el popup
+function cerrarPopup() {
+    document.getElementById("popupContainer").style.display = "none";
+}
+
+// Función para simular la confirmación del pedido
+function confirmarPedido() {
+    var mesaSelect = document.getElementById("mesaSelect");
+    var mesaSeleccionada = mesaSelect.options[mesaSelect.selectedIndex].text;
+    alert("Pedido confirmado para la mesa: " + mesaSeleccionada);
+    cerrarPopup();
+}
+
+function validarCampos() {
+    var usuario = document.getElementById("usuario").value;
+    var contrasena = document.getElementById("contrasena").value;
+
+    if (usuario.trim() === "" || contrasena.trim() === "") {
+        alert("Por favor, ingresa un usuario y contraseña válidos.");
+        return false;
+    }
+
+    // Simulación de inicio de sesión exitoso
+    var esAdmin = usuario === "admin" && contrasena === "admin123";
+    
+    if (esAdmin) {
+        alert("Inicio de sesión exitoso como administrador.");
+        window.location.href = "../VIEWS/admin.html"; // Redirigir a la página de administrador
+    } else {
+        alert("Inicio de sesión exitoso como usuario.");
+        window.location.href = "../VIEWS/TomaPedido.html"; // Redirigir a la página de usuario
+    }
+
+    return false; // Evitar el envío del formulario por ahora
+}
